1d4196ac2184cff286b93d8528ca78ebf4302212
Luis Dalmolin <luis.nh@gmail.com>
2018-03-14 20:58:43 -0300

    First commit

---
+++ resources/assets/js/components/ExampleComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <p><strong>Translation for "auth.php" (PHP file):</strong><br>
+        {{ $t('auth.failed') }}</p>
+
+        <p><strong>Translation for "Hello World" (JSON file):</strong><br>
+        {{ $t('Hello World') }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
